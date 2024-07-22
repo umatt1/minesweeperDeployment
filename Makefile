1ea63c08803a74b195cb73d9538f6edd522a7f50
umatt1 <matt.burkhard@gmail.com>
2024-07-22 17:25:57 -0500

    Copy paste some RDS module code

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ plan:
 	terraform plan -var-file=fixtures.us-east-2.tfvars
 apply:
 	terraform apply -var-file=fixtures.us-east-2.tfvars
+destroy:
+	terraform destroy -var-file=fixtures.us-east-2.tfvars
--- main.tf
+++ main.tf
@@ -103,3 +103,57 @@ module "ecs_alb_service_task" {
 
   context = module.this.context
 }
+
+module "rds_instance" {
+    source = "cloudposse/rds/aws"
+    # Cloud Posse recommends pinning every module to a specific version
+    # version = "x.x.x"
+    namespace                   = "eg"
+    stage                       = "prod"
+    name                        = "app"
+    dns_zone_id                 = "Z89FN1IW975KPE"
+    host_name                   = "db"
+    security_group_ids          = ["sg-xxxxxxxx"]
+    ca_cert_identifier          = "rds-ca-2019"
+    allowed_cidr_blocks         = ["XXX.XXX.XXX.XXX/32"]
+    database_name               = "wordpress"
+    database_user               = "admin"
+    database_password           = "xxxxxxxxxxxx"
+    database_port               = 3306
+    multi_az                    = true
+    storage_type                = "gp2"
+    allocated_storage           = 100
+    storage_encrypted           = true
+    engine                      = "mysql"
+    engine_version              = "5.7.17"
+    major_engine_version        = "5.7"
+    instance_class              = "db.t2.medium"
+    db_parameter_group          = "mysql5.7"
+    option_group_name           = "mysql-options"
+    publicly_accessible         = false
+    subnet_ids                  = ["sb-xxxxxxxxx", "sb-xxxxxxxxx"]
+    vpc_id                      = "vpc-xxxxxxxx"
+    snapshot_identifier         = "rds:production-2015-06-26-06-05"
+    auto_minor_version_upgrade  = true
+    allow_major_version_upgrade = false
+    apply_immediately           = false
+    maintenance_window          = "Mon:03:00-Mon:04:00"
+    skip_final_snapshot         = false
+    copy_tags_to_snapshot       = true
+    backup_retention_period     = 7
+    backup_window               = "22:00-03:00"
+
+    db_parameter = [
+      { name  = "myisam_sort_buffer_size"   value = "1048576" },
+      { name  = "sort_buffer_size"          value = "2097152" }
+    ]
+
+    db_options = [
+      { option_name = "MARIADB_AUDIT_PLUGIN"
+          option_settings = [
+            { name = "SERVER_AUDIT_EVENTS"           value = "CONNECT" },
+            { name = "SERVER_AUDIT_FILE_ROTATIONS"   value = "37" }
+          ]
+      }
+    ]
+}
